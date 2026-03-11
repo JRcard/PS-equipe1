54ac44960cb3b9384c2dc9ef0d5f0a85d57a62f1
ATTP1 <allantritampham@gmail.com>
2026-03-11 13:51:53 -0400

    Création footer et ajout du import dans App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
-import { RouterView } from 'vue-router'
-import Home from './components/Home.vue';
+import { RouterView } from "vue-router";
+import Home from "./components/Home.vue";
+
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
   <RouterView />
+  <Footer />
 </template>
 
 <style scoped></style>
