2de52ab2f01be883432ecff3e39d4ca3502acc27
JRcard <jrm.ricard@gmail.comm>
2026-03-16 20:53:07 -0400

    Mise en place d'un table et tests d'affichage

--- src/components/BudgetMensuel.vue
+++ src/components/BudgetMensuel.vue
@@ -6,6 +6,42 @@
 			<p>{{ data }}</p>
 		</div>
 		<div><button @click="nouveauMois(data)">Nouveau mois</button></div>
+		<div>
+			<h2>Budget mensuel</h2>
+			<h3>Revenus</h3>
+			<table>
+				<thead>
+					<tr>
+						<th>Description</th>
+						<th>Revenu mensuel</th>
+						<th>Récurrent</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr v-for="transaction in data" :key="transaction.id">
+						<td>{{ transaction.description }}</td>
+						<td>{{ transaction.amount }}</td>
+						<td>{{ transaction.frequency }}</td>
+						<td @click="del(transaction.id)">supprime</td>
+					</tr>
+					<tr>
+						<td>cell1_2</td>
+						<td>cell2_2</td>
+						<td>cell3_2</td>
+						<td>cell4_2</td>
+					</tr>
+				</tbody>
+				<tfoot>
+					<tr>
+						<td>foot1</td>
+						<td>foot2</td>
+						<td>foot3</td>
+						<td>foot4</td>
+					</tr>
+				</tfoot>
+			</table>
+		</div>
 	</div>
 </template>
 
