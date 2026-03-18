49ae89293266c853c570556c9010c04692c942a0
Lucas Minville <lucas_minville@hotmail.com>
2026-03-17 20:02:09 -0400

    Ajout du put pour modifier le user

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -6,7 +6,7 @@
         <div class="flex justify-between">
             <h2 class="underline">Detail du profil</h2>
             <button v-if="isDisable" @click="toggleEdit()">Mettre à jour</button>
-            <button v-else @click="toggleEdit()">Enregister</button>
+            <button v-else @click="handleSubmit()">Enregister</button>
         </div>
         <div>
             <form @submit.prevent="handleSubmit">
@@ -14,28 +14,28 @@
                     <legend>Information personnels</legend>
                     <div>
                         <label for="firstName" hidden>firstName</label>
-                        <input type="text" name="firstName" id="firstName" :value="userData.firstName"
+                        <input type="text" v-model="userData.firstName" name="firstName" id="firstName"
                             :disabled="isDisable" placeholder="Prénom">
                     </div>
                     <div>
                         <label for="lastName" hidden>lastName</label>
-                        <input type="text" name="lastName" id="lastName" :value="userData.lastName"
+                        <input type="text" v-model="userData.lastName" name="lastName" id="lastName"
                             :disabled="isDisable" placeholder="Nom">
                     </div>
                     <div>
                         <label for="birthDate" hidden>birthDate</label>
-                        <input type="date" name="birthDate" id="birthDate" :value="userData.birthDate"
+                        <input type="date" v-model="userData.birthDate" name="birthDate" id="birthDate"
                             :disabled="isDisable" placeholder="Nom">
                     </div>
                     <div>
                         <label for="email" hidden>email</label>
-                        <input type="text" name="email" id="email" :value="userData.email" :disabled="isDisable"
+                        <input type="text" v-model="userData.email" name="email" id="email" :disabled="isDisable"
                             placeholder="Email">
                     </div>
                     <div>
                         <label for="password" hidden>password</label>
-                        <input :type="showPassword ? 'password' : 'text'" name="password" id="password"
-                            :value="userData.password" :disabled="isDisable" placeholder="Email">
+                        <input :type="showPassword ? 'password' : 'text'" v-model="userData.password" name="password"
+                            id="password" :disabled="isDisable" placeholder="Email">
                         <button @click="togglePasswordShow">Show Password</button>
                     </div>
                 </fieldset>
@@ -60,6 +60,7 @@
 </template>
 
 <script setup>
+import { Profil } from '@/services/profilService';
 import { onMounted, ref } from 'vue';
 
 const connectUserOne = () => {
@@ -80,7 +81,13 @@ const toggleEdit = () => {
     isDisable.value = !isDisable.value
 }
 
-const userData = ref([]);
+const userData = ref({
+    firstName: "",
+    lastName: "",
+    birthDate: "",
+    email: "",
+    password: "",
+});
 
 
 const fetchUser = async () => {
@@ -90,16 +97,36 @@ const fetchUser = async () => {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        userData.value = data
+        userData.value.firstName = data.firstName
+        userData.value.lastName = data.lastName
+        userData.value.email = data.email
+        userData.value.birthDate = data.birthDate
+        userData.value.password = data.password
+        console.log(userData)
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
     }
 }
 
+const handleSubmit = async () => {
 
+    try {
+        const response = await fetch(`https://money-pie-1.fly.dev/api/v1/users/${localStorage.getItem("userId")}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(userData.value),
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
 
-const handleSubmit = () => {
-
+        const data = await response.json();
+        console.log('Success:', data);
+    } catch (err) {
+        console.error('Error:', err);
+    }
 }
 
 onMounted(() => {
