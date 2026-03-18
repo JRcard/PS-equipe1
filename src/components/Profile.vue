6c1184d8904f0628f4a2e4d07a1d0794553754b1
Lucas Minville <lucas_minville@hotmail.com>
2026-03-17 21:00:55 -0400

    Ajout de foncion pour voir et modifier les adresses

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -40,14 +40,70 @@
                     </div>
                 </fieldset>
 
-                <fieldset class="border-2">
+                <fieldset class="border-2 flex justify-around">
                     <legend>Adresses</legend>
                     <div>
-                        <h3>Maison</h3>
+                        <h3 v-if="userAdresseOne.type == 'PERSONAL'">Maison</h3>
+                        <h3 v-else>Travail</h3>
                         <div>
-                            <label for="firstName" hidden>firstName</label>
-                            <input type="text" name="firstName" id="firstName" :value="userData.firstName"
-                                :disabled="isDisable" placeholder="Prénom">
+                            <label for="streetNumber" hidden>streetNumber</label>
+                            <input type="text" name="streetNumber" id="streetNumber"
+                                v-model="userAdresseOne.streetNumber" :disabled="isDisable" placeholder="streetNumber">
+                        </div>
+                        <div>
+                            <label for="streetName" hidden>streetNumber</label>
+                            <input type="text" name="streetName" id="streetName" v-model="userAdresseOne.streetName"
+                                :disabled="isDisable" placeholder="streetName">
+                        </div>
+                        <div>
+                            <label for="city" hidden>streetNumber</label>
+                            <input type="text" name="city" id="city" v-model="userAdresseOne.city" :disabled="isDisable"
+                                placeholder="city">
+                        </div>
+                        <div>
+                            <label for="province" hidden>streetNumber</label>
+                            <input type="text" name="province" id="province" v-model="userAdresseOne.province"
+                                :disabled="isDisable" placeholder="province">
+                        </div>
+                        <div>
+                            <label for="country" hidden>streetNumber</label>
+                            <input type="text" name="country" id="country" v-model="userAdresseOne.country"
+                                :disabled="isDisable" placeholder="country">
+                        </div>
+                    </div>
+
+                    <div>
+                        <h3 v-if="userAdresseOne.type == 'PERSONAL'">Travail</h3>
+                        <h3 v-else>Maison</h3>
+                        <div>
+                            <label for="streetNumber" hidden>streetNumber</label>
+                            <input type="text" name="streetNumber" id="streetNumber"
+                                v-model="userAdresseTwo.streetNumber" :disabled="isDisable" placeholder="streetNumber">
+                        </div>
+                        <div>
+                            <label for="streetName" hidden>streetNumber</label>
+                            <input type="text" name="streetName" id="streetName" v-model="userAdresseTwo.streetName"
+                                :disabled="isDisable" placeholder="streetName">
+                        </div>
+                        <div>
+                            <label for="city" hidden>streetNumber</label>
+                            <input type="text" name="city" id="city" v-model="userAdresseTwo.city" :disabled="isDisable"
+                                placeholder="city">
+                        </div>
+                        <div>
+                            <label for="province" hidden>streetNumber</label>
+                            <input type="text" name="province" id="province" v-model="userAdresseTwo.province"
+                                :disabled="isDisable" placeholder="province">
+                        </div>
+                        <div>
+                            <label for="country" hidden>streetNumber</label>
+                            <input type="text" name="country" id="country" v-model="userAdresseTwo.country"
+                                :disabled="isDisable" placeholder="country">
+                        </div>
+                        <div>
+                            <label for="type" hidden>type</label>
+                            <input type="text" name="type" id="type" v-model="userAdresseTwo.type" :disabled="isDisable"
+                                placeholder="type" hidden>
                         </div>
                     </div>
 
@@ -60,7 +116,6 @@
 </template>
 
 <script setup>
-import { Profil } from '@/services/profilService';
 import { onMounted, ref } from 'vue';
 
 const connectUserOne = () => {
@@ -88,8 +143,6 @@ const userData = ref({
     email: "",
     password: "",
 });
-
-
 const fetchUser = async () => {
     try {
         const response = await fetch(`https://money-pie-1.fly.dev/api/v1/users/${localStorage.getItem("userId")}`)
@@ -108,15 +161,55 @@ const fetchUser = async () => {
     }
 }
 
-const handleSubmit = async () => {
 
+const userAdresseOne = ref({
+    streetNumber: "",
+    streetName: "",
+    city: "",
+    province: "",
+    country: "",
+    type: "PERSONAL"
+})
+const userAdresseTwo = ref({
+    streetNumber: "",
+    streetName: "",
+    city: "",
+    province: "",
+    country: "",
+    type: "WORK"
+})
+
+const setAddresses = (addresses, data, index) => {
+    addresses.value.streetNumber = data[index].streetNumber
+    addresses.value.streetName = data[index].streetName
+    addresses.value.city = data[index].city
+    addresses.value.province = data[index].province
+    addresses.value.country = data[index].country
+    addresses.value.type = data[index].type
+}
+const fetchUserAdresse = async () => {
     try {
-        const response = await fetch(`https://money-pie-1.fly.dev/api/v1/users/${localStorage.getItem("userId")}`, {
+        const response = await fetch(`https://money-pie-1.fly.dev/api/v1/users/${localStorage.getItem("userId")}/addresses`)
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setAddresses(userAdresseOne, data, 0)
+        setAddresses(userAdresseTwo, data, 1)
+        console.log(data)
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+}
+
+const putFunction = async (url, dataToSend) => {
+    try {
+        const response = await fetch(url, {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(userData.value),
+            body: JSON.stringify(dataToSend.value),
         });
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
@@ -124,13 +217,23 @@ const handleSubmit = async () => {
 
         const data = await response.json();
         console.log('Success:', data);
+        toggleEdit()
     } catch (err) {
         console.error('Error:', err);
     }
 }
 
+
+const handleSubmit = () => {
+    putFunction(`https://money-pie-1.fly.dev/api/v1/users/${localStorage.getItem("userId")}`, userData);
+    putFunction(`https://money-pie-1.fly.dev/api/v1/users/${localStorage.getItem("userId")}/addresses`, userAdresseOne)
+    putFunction(`https://money-pie-1.fly.dev/api/v1/users/${localStorage.getItem("userId")}/addresses`, userAdresseTwo)
+
+}
+
 onMounted(() => {
     fetchUser();
+    fetchUserAdresse();
 })
 
 </script>
