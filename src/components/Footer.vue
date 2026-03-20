6c372b45d74d21651370fdaf1f0f3edf3caf7b4a
ATTP1 <allantritampham@gmail.com>
2026-03-20 00:03:00 -0400

    Finalisation footer: ajout boxing pour image et info, ajout fade gradient, ajout logo, creation dossier assets/images

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,21 @@
 <template>
   <footer>
-    <p>EQUIPE 1 FOOTER</p>
+    <div class="h-[5vh] bg-linear-to-t from-indigo-200 to-transparent"></div>
+    <div class="bg-indigo-200 h-[15vh] flex items-center justify-between px-20">
+      <img class="h-[15vh]" :src="logo" alt="Logo Stratos" />
+
+      <h3>
+        Projet conçu et réalisé par :
+        <br />
+        <strong class="text-violet-700"
+          >Marie-Pier Simard, Lucas Minville,<br />
+          Jérémie Ricard & Allan Tri Tam Pham.</strong
+        >
+      </h3>
+    </div>
   </footer>
 </template>
 
-<script setup></script>
+<script setup>
+import logo from "../assets/images/stratosLogo.png";
+</script>
