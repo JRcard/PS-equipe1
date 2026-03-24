722c1552983b3100e70621bb647c8ffdd81ab517
Lucas Minville <lucas_minville@hotmail.com>
2026-03-24 19:58:35 -0400

    modification du style.css pour que les pc avec touchscreen puisse avoir des effect hover, ajout au profil des sections details

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -153,18 +153,83 @@
                     </div>
                 </div>
 
-                <div class="bg-card p-8 rounded-2xl shadow-xl border border-white/5 w-[49%] flex flex-col gap-6">
-                    <div>
-                        <h2 class="text-2xl font-bold mb-1">Renseignements scolaires</h2>
-                        <div>
-                            <div>
-                                <label for="streetNumber" hidden>streetNumber</label>
-                                <input type="text" name="streetNumber" id="streetNumber"
-                                    v-model="schoolDetail.schoolName" :disabled="isDisable" placeholder="streetNumber"
-                                    class="w-full bg-input-bg border border-input-border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300 focus:border-secondaire focus:ring-1 focus:ring-secondaire">
+                <div>
+                    <h2 class="text-2xl font-bold mb-1">Renseignements</h2>
+                    <div class="flex justify-between">
+
+                        <div
+                            class="bg-card p-8 rounded-2xl shadow-xl border border-white/5 w-[49%] flex flex-col gap-6">
+                            <h2 class="text-2xl font-bold mb-1">Renseignements scolaires</h2>
+                            <div class="flex flex-col gap-6">
+                                <div>
+                                    <label for="schoolName" hidden>schoolName</label>
+                                    <input type="text" name="streetNumber" id="schoolName"
+                                        v-model="schoolDetail.schoolName" :disabled="isDisable" placeholder="schoolName"
+                                        class="w-full bg-input-bg border border-input-border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300 focus:border-secondaire focus:ring-1 focus:ring-secondaire">
+                                </div>
+                                <div>
+                                    <label for="fieldOfStudy" hidden>fieldOfStudy</label>
+                                    <input type="text" name="fieldOfStudy" id="fieldOfStudy"
+                                        v-model="schoolDetail.fieldOfStudy" :disabled="isDisable"
+                                        placeholder="fieldOfStudy"
+                                        class="w-full bg-input-bg border border-input-border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300 focus:border-secondaire focus:ring-1 focus:ring-secondaire">
+                                </div>
+                                <div>
+                                    <label for="startDate" hidden>startDate</label>
+                                    <input type="date" v-model="schoolDetail.startDate" name="startDate" id="startDate"
+                                        :disabled="isDisable" placeholder="startDate"
+                                        class="w-full bg-input-bg border border-input-border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300 focus:border-secondaire focus:ring-1 focus:ring-secondaire">
+                                </div>
+
+                                <div>
+                                    <label for="projectedEndDate" hidden>projectedEndDate</label>
+                                    <input type="date" name="projectedEndDate" id="projectedEndDate"
+                                        v-model="schoolDetail.projectedEndDate" :disabled="isDisable"
+                                        placeholder="projectedEndDate"
+                                        class="w-full bg-input-bg border border-input-border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300 focus:border-secondaire focus:ring-1 focus:ring-secondaire">
+                                </div>
+                                <button type="button" @click="delDetailFunction('school-details')"
+                                    :class="isDisable ? 'hidden' : ''"
+                                    class="px-3.5 py-2.5 rounded-md font-semibold text-white flex items-center justify-center transition-all duration-300 bg-red-800 hover:shadow-[0_0_15px_#9034b080,0_0_15px_#096cfd80]">Supprimer</button>
+
 
                             </div>
+                        </div>
 
+                        <div
+                            class="bg-card p-8 rounded-2xl shadow-xl border border-white/5 w-[49%] flex flex-col gap-6">
+                            <h2 class="text-2xl font-bold mb-1">Renseignements bancaires</h2>
+                            <div class="flex flex-col gap-6">
+                                <div>
+                                    <label for="institutionName" hidden>institutionName</label>
+                                    <input type="text" name="institutionNameinstitutionName" id="institutionName"
+                                        v-model="bankDetail.institutionName" :disabled="isDisable"
+                                        placeholder="institutionName"
+                                        class="w-full bg-input-bg border border-input-border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300 focus:border-secondaire focus:ring-1 focus:ring-secondaire">
+                                </div>
+                                <div>
+                                    <label for="accountInfo" hidden>accountInfo</label>
+                                    <input type="text" name="accountInfo" id="accountInfo"
+                                        v-model="bankDetail.accountInfo" :disabled="isDisable" placeholder="accountInfo"
+                                        class="w-full bg-input-bg border border-input-border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300 focus:border-secondaire focus:ring-1 focus:ring-secondaire">
+                                </div>
+                                <div>
+                                    <label for="loanInfo" hidden>startDate</label>
+                                    <input type="text" name="loanInfo" id="loanInfo" v-model="bankDetail.loanInfo"
+                                        :disabled="isDisable" placeholder="loanInfo"
+                                        class="w-full bg-input-bg border border-input-border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300 focus:border-secondaire focus:ring-1 focus:ring-secondaire">
+                                </div>
+                                <div>
+                                    <label for="other" hidden>other</label>
+                                    <input type="text" name="other" id="other" v-model="bankDetail.other"
+                                        :disabled="isDisable" placeholder="other"
+                                        class="w-full bg-input-bg border border-input-border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300 focus:border-secondaire focus:ring-1 focus:ring-secondaire">
+                                </div>
+                                <button type="button" @click="delDetailFunction('banking-details')"
+                                    :class="isDisable ? 'hidden' : ''"
+                                    class="px-3.5 py-2.5 rounded-md font-semibold text-white flex items-center justify-center transition-all duration-300 bg-red-800 hover:shadow-[0_0_15px_#9034b080,0_0_15px_#096cfd80]">Supprimer</button>
+
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -203,6 +268,7 @@ const userData = ref({
     email: "",
     password: "",
 });
+
 const fetchUser = async () => {
     try {
         const response = await fetch(`https://money-pie-1.fly.dev/api/v1/users/${localStorage.getItem("userId")}`)
@@ -247,7 +313,14 @@ const schoolDetail = ref({
     projectedEndDate: ""
 })
 
-const setAddress = (target, data) => {
+const bankDetail = ref({
+    institutionName: "",
+    accountInfo: "",
+    loanInfo: "",
+    other: ""
+})
+
+const setData = (target, data) => {
     target.value = { ...data };
 }
 
@@ -265,15 +338,90 @@ const fetchUserAdresse = async () => {
         const personal = data.find(a => a.type === "PERSONAL");
         const work = data.find(a => a.type === "WORK");
 
-        if (personal) setAddress(userAdresseOne, personal);
-        if (work) setAddress(userAdresseTwo, work);
+        if (personal) setData(userAdresseOne, personal);
+        if (work) setData(userAdresseTwo, work);
 
     } catch (error) {
         console.error(error);
     }
 }
 
+const fetchUserDetails = async (type) => {
+    try {
+        const response = await fetch(`https://money-pie-1.fly.dev/api/v1/users/${localStorage.getItem("userId")}/${type}`)
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+
+
+        if (type === "banking-details") {
+            bankDetail.value = {
+                institutionName: data.institutionName,
+                accountInfo: data.accountInfo,
+                loanInfo: data.loanInfo,
+                other: data.other
+            }
+        }
+        if (type === "school-details") {
+            schoolDetail.value = {
+                schoolName: data.schoolName,
+                fieldOfStudy: data.fieldOfStudy,
+                startDate: data.startDate,
+                projectedEndDate: data.projectedEndDate
+            }
+        }
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const delDetailFunction = async (type) => {
+    try {
+        const response = await fetch(`https://money-pie-1.fly.dev/api/v1/users/${localStorage.getItem("userId")}/${type}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        if (type === "school-details") {
+            schoolDetail.value = {
+                schoolName: "",
+                fieldOfStudy: "",
+                startDate: "",
+                projectedEndDate: ""
+            }
+        }
+
+        if (type === "banking-details") {
+            bankDetail.value = {
+                institutionName: "",
+                accountInfo: "",
+                loanInfo: "",
+                other: ""
+            }
+        }
+
+
+        fetchUserAdresse()
+        const data = await response.json();
+        console.log('Success:', data);
+
+    } catch (err) {
+        console.error('Error:', err);
+    }
+
+}
+
+
 const delAdressFunction = async (type) => {
+
     try {
         const response = await fetch(`https://money-pie-1.fly.dev/api/v1/users/${localStorage.getItem("userId")}/addresses/${type}`, {
             method: 'DELETE',
@@ -345,6 +493,10 @@ const handleSubmit = () => {
     putFunction(`https://money-pie-1.fly.dev/api/v1/users/${localStorage.getItem("userId")}`, userData);
     putFunction(`https://money-pie-1.fly.dev/api/v1/users/${localStorage.getItem("userId")}/addresses`, userAdresseOne);
     putFunction(`https://money-pie-1.fly.dev/api/v1/users/${localStorage.getItem("userId")}/addresses`, userAdresseTwo);
+    putFunction(`https://money-pie-1.fly.dev/api/v1/users/${localStorage.getItem("userId")}/banking-details`, bankDetail);
+    putFunction(`https://money-pie-1.fly.dev/api/v1/users/${localStorage.getItem("userId")}/school-details`, schoolDetail);
+
+
     toggleEdit();
     if (showPassword.value === false) {
         togglePasswordShow()
@@ -354,6 +506,9 @@ const handleSubmit = () => {
 onMounted(() => {
     fetchUser();
     fetchUserAdresse();
+    fetchUserDetails("school-details");
+    fetchUserDetails("banking-details");
+
 })
 
 </script>
