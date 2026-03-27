dcf82001446fd684e63850003a62444e3c271952
ATTP1 <allantritampham@gmail.com>
2026-03-27 01:32:07 -0400

    Creation de la page 404: Ajout du routing 404, Style baser sur Home, Ajout d'un link pour retour Accueil

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import BudgetMensuel from "@/components/BudgetMensuel.vue";
 import Home from "@/components/Home.vue";
 import Profile from "@/components/Profile.vue";
+import Page404 from "@/components/Page404.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
-
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
@@ -18,12 +18,16 @@ const router = createRouter({
       component: Profile,
     },
     {
-			path: "/budget",
-			name: "budgetMensuel",
-			component: BudgetMensuel,
-		},
+      path: "/budget",
+      name: "budgetMensuel",
+      component: BudgetMensuel,
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "Page404",
+      component: Page404,
+    },
   ],
-
 });
 
 export default router;
