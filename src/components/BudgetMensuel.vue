f5b60ca89da9e217ab7fab8a53e7e72cdf966384
JRcard <jrm.ricard@gmail.comm>
2026-03-29 22:53:35 -0400

    mise en place du tableau dépense avec toute ses fonctions

--- src/components/BudgetMensuel.vue
+++ src/components/BudgetMensuel.vue
@@ -1,23 +1,26 @@
 <template>
-	<div>
-		<div>
-			<h3>Votre balance ce mois-ci :</h3>
-			<p>+ 500$</p>
-			<p>{{ data }}</p>
+	<div class="max-w-475 mx-auto mt-10">
+		<div class="flex text-2xl font-bold">
+			<h3 class="mr-2 mb-10">Votre balance ce mois-ci :</h3>
+			<p v-if="balance > 0">{{ balance }}$</p>
+			<p v-else class="text-red-500">{{ balance }}$</p>
 		</div>
-		<div><button @click="nouveauMois()">Nouveau mois</button></div>
+		<div></div>
 		<div>
-			<h2>Budget mensuel</h2>
+			<div class="flex justify-between my-10">
+				<h2 class="text-4xl font-bold mb-1">Budget mensuel</h2>
+				<button @click="nouveauMois()" class="px-8.5 py-2.5 rounded-md font-semibold text-white flex items-center justify-center transition-all duration-300 bg-linear-to-r from-principale to-secondaire hover:shadow-[0_0_15px_#9034b080,0_0_15px_#096cfd80]">NOUVEAU MOIS</button>
+			</div>
 			<div class="flex gap-3 justify-center">
-				<TableauRevenu @revenuDeleteID="deleteRevenu" @newRevenu="ajoutRevenu" @updatedRevenu="updatedRevenu" @editThisRow="editThisRow" :data="revenuList" :editingRow="currentEditing" />
-				<TableauDepense :data="depenseList" />
+				<TableauRevenu @totalRevenu="totalRevenu = $event" @deleteID="deleteTransaction" @newTransaction="ajoutTransaction" @updateTransaction="updateTransaction" @editThisRow="editThisRow" @stopEditing="stopEditing" :data="revenuList" :editingRow="currentEditing" />
+				<TableauDepense @totalExpense="totalExpense = $event" @deleteID="deleteTransaction" @newTransaction="ajoutTransaction" @updateTransaction="updateTransaction" @editThisRow="editThisRow" @stopEditing="stopEditing" :data="depenseList" :editingRow="currentEditing" />
 			</div>
 		</div>
 	</div>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import TableauRevenu from "./tableauRevenu.vue";
 import TableauDepense from "./tableauDepense.vue";
 
@@ -27,6 +30,9 @@ const revenuList = ref([]);
 const depenseList = ref([]);
 const data = ref([]);
 const currentEditing = ref([]);
+const totalRevenu = ref(0);
+const totalExpense = ref(0);
+const balance = computed(() => totalRevenu.value - totalExpense.value);
 
 const useAPI = (url) => {
 	fetch(url)
@@ -34,7 +40,7 @@ const useAPI = (url) => {
 		.then((json) => {
 			data.value = json;
 			revenuList.value = json.filter((t) => t.type === "Revenue").sort((a, b) => a.id - b.id);
-			depenseList.value = json.filter((t) => t.type === "Expense");
+			depenseList.value = json.filter((t) => t.type === "Expense").sort((a, b) => a.id - b.id);
 		})
 		.catch((err) => {
 			console.log("Erreur: " + err + " | " + url);
@@ -57,8 +63,12 @@ const nouveauMois = async () => {
 const editThisRow = (id) => {
 	currentEditing.value = data.value.find((t) => t.id === id);
 };
+const stopEditing = () => {
+	currentEditing.value = [];
+	useAPI(url);
+};
 
-const ajoutRevenu = async (load) => {
+const ajoutTransaction = async (load) => {
 	try {
 		const response = await fetch(url, {
 			method: "POST",
@@ -72,15 +82,13 @@ const ajoutRevenu = async (load) => {
 		}
 
 		const newRow = await response.json();
-		revenuList.value.push(newRow);
 		currentEditing.value = newRow;
-		console.log("Success:", newRow);
 	} catch (err) {
 		console.error("Error:", err);
 	}
 	useAPI(url);
 };
-const updatedRevenu = async (load) => {
+const updateTransaction = async (load) => {
 	if (load.id == undefined) {
 		return;
 	}
@@ -100,10 +108,9 @@ const updatedRevenu = async (load) => {
 	}
 	useAPI(url);
 };
-const deleteRevenu = async (id) => {
+const deleteTransaction = async (id) => {
 	try {
 		await fetch(`${url}/${id}`, { method: "DELETE" });
-		console.log("Renevu supprimé");
 	} catch (err) {
 		console.log("Erreur: " + err + " | " + url);
 	}
