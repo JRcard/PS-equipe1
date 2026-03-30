9c3a48821d109ef004cec6ca09dc54d69aca3747
rochequiroule <marie.pier.simard.01@edu.cegeptr.qc.ca>
2026-03-30 09:47:14 -0400

    modification link routerlink dans le SideBar

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -12,14 +12,14 @@
       </div>
 
       <div class="space-y-2">
-        <RouterLink to="/budget" class="-mx-3 px-3 py-2 text-base/7 flex gap-6 text-white hover:border-l-3 border-secondaire hover:font-semibold"
+        <RouterLink to="/profil" class="-mx-3 px-3 py-2 text-base/7 flex gap-6 text-white hover:border-l-3 border-secondaire hover:font-semibold"
           ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
           </svg>
           Profil</RouterLink
         >
 
-        <RouterLink to="/profil" class="-mx-3 px-3 py-2 flex text-base/7 gap-6 text-white hover:border-l-3 border-secondaire hover:font-semibold"
+        <RouterLink to="/budget" class="-mx-3 px-3 py-2 flex text-base/7 gap-6 text-white hover:border-l-3 border-secondaire hover:font-semibold"
           ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
           </svg>
