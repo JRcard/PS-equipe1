6e9bdde617542fe2b372fb3f19f68083f7591c13
Lucas Minville <lucas_minville@hotmail.com>
2026-04-01 20:57:39 -0400

    Creation des fonctionnaliter de creation de compte, ajout a la nav les fonctionnaliter pour faire apparaitre le modal

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -8,30 +8,38 @@
         </a>
       </div>
       <div class="flex lg:hidden">
-        <button type="button" command="show-modal" commandfor="mobile-menu" class="-m-2.5 inline-flex items-center rounded-md p-2.5 text-gray-200">
+        <button type="button" command="show-modal" commandfor="mobile-menu"
+          class="-m-2.5 inline-flex items-center rounded-md p-2.5 text-gray-200">
           <span class="sr-only">Ouvrir menu</span>
-          <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6">
+          <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon"
+            aria-hidden="true" class="size-6">
             <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" stroke-linecap="round" stroke-linejoin="round" />
           </svg>
         </button>
       </div>
       <div class="hidden lg:flex lg:gap-x-12">
         <a href="#" class="text-base/7 font-semibold text-white">Se connecter</a>
-        <a href="#" class="text-base/7 font-semibold text-white">S'inscrire</a>
+        <a class="text-base/7 font-semibold text-white" @click="isOpen = true">
+          S'inscrire
+        </a>
       </div>
+
     </nav>
     <el-dialog>
       <dialog id="mobile-menu" class="backdrop:bg-transparent lg:hidden">
         <div tabindex="0" class="fixed inset-0 focus:outline-none">
-          <el-dialog-panel class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-background p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
+          <el-dialog-panel
+            class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-background p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
             <div class="flex items-center justify-between">
               <a href="#" class="-m-1.5 p-1.5">
                 <span class="sr-only">Stratos</span>
                 <img src="../assets/statos-logo-icon.png" alt="Stratos logo" class="h-8 w-auto" />
               </a>
-              <button type="button" command="close" commandfor="mobile-menu" class="-m-2.5 rounded-md p-2.5 text-gray-200">
+              <button type="button" command="close" commandfor="mobile-menu"
+                class="-m-2.5 rounded-md p-2.5 text-gray-200">
                 <span class="sr-only">Fermer menu</span>
-                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6">
+                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon"
+                  aria-hidden="true" class="size-6">
                   <path d="M6 18 18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
               </button>
@@ -39,8 +47,11 @@
             <div class="mt-6 flow-root">
               <div class="-my-6 divide-y divide-white/10">
                 <div class="space-y-2 py-6">
-                  <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5">Se connecter</a>
-                  <a href="#" class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5">S'inscrire</a>
+                  <a href="#"
+                    class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5">Se
+                    connecter</a>
+                  <a class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
+                    @click="isOpen = true">S'inscrire</a>
                 </div>
               </div>
             </div>
@@ -49,4 +60,15 @@
       </dialog>
     </el-dialog>
   </header>
+
+  <Inscription :isOpen="isOpen" @close="isOpen = false" />
 </template>
+
+
+<script setup>
+import { ref } from 'vue'
+import Inscription from './inscription.vue';
+
+
+const isOpen = ref(false)
+</script>
--- src/components/inscription.vue
+++ src/components/inscription.vue
@@ -1,47 +1,63 @@
 <template>
-    <div class="bg-background text-sm/6 flex items-center justify-center">
-        <!-- card-->
-        <div class="bg-card p-8 rounded-2xl shadow-xl border border-white/5 w-full max-w-md flex flex-col gap-6">
-            <div>
-                <h2 class="text-2xl font-bold mb-1">Inscription</h2>
-                <p class="text-text-secondaire text-sm">Entrez vos informations ci-dessous.</p>
-            </div>
+    <div v-if="isOpen" class="fixed inset-0 z-40 flex items-center justify-center bg-black/30 backdrop-blur-sm"
+        @click.self="emit('close')">
 
-            <!-- imput -->
-            <form class="flex flex-col gap-4" @submit.prevent="handleSubmit">
-                <input type="email" placeholder="Adresse email" v-model="inscData.email"
-                    class="w-full bg-input-bg border border-input-border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300 focus:border-secondaire focus:ring-1 focus:ring-secondaire" />
+        <div class="bg-background rounded-2xl text-sm/6 flex items-center justify-center">
+            <!-- card-->
+            <div class="bg-card p-8 rounded-2xl shadow-xl border border-white/5 w-full max-w-md flex flex-col gap-6">
+                <div>
+                    <h2 class="text-2xl font-bold mb-1">Inscription</h2>
+                    <p class="text-text-secondaire text-sm">Entrez vos informations ci-dessous.</p>
+                </div>
 
-                <div class="flex gap-3">
-                    <input :type="showPassword ? 'password' : 'text'" placeholder="Mot de passe"
-                        v-model="inscData.password"
+                <!-- imput -->
+                <form class="flex flex-col gap-4" novalidate @submit.prevent="handleSubmit">
+                    <input type="email" placeholder="Adresse email" v-model="inscData.email" :class="[
+                        'w-full bg-input-bg border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300',
+                        errors.email ? 'border-red-500' : 'border-input-border'
+                    ]"
                         class="w-full bg-input-bg border border-input-border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300 focus:border-secondaire focus:ring-1 focus:ring-secondaire" />
 
-                    <button type="button" @click="togglePasswordShow"
-                        class="px-4 rounded-md bg-principale hover:opacity-80">
-                        👁
-                    </button>
-                </div>
-                <input type="text" placeholder="Prénom" v-model="inscData.firstName"
-                    class="w-full bg-input-bg border border-input-border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300 focus:border-secondaire focus:ring-1 focus:ring-secondaire" />
+                    <div class="flex gap-3">
+                        <input :type="showPassword ? 'password' : 'text'" placeholder="Mot de passe"
+                            v-model="inscData.password" :class="[
+                                'w-full bg-input-bg border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300',
+                                errors.password ? 'border-red-500' : 'border-input-border'
+                            ]"
+                            class="w-full bg-input-bg border border-input-border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300 focus:border-secondaire focus:ring-1 focus:ring-secondaire" />
 
-                <input type="text" placeholder="Nom" v-model="inscData.lastName"
-                    class="w-full bg-input-bg border border-input-border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300 focus:border-secondaire focus:ring-1 focus:ring-secondaire" />
+                        <button type="button" @click="togglePasswordShow"
+                            class="px-4 rounded-md bg-principale hover:opacity-80">
+                            👁
+                        </button>
+                    </div>
+                    <input type="text" placeholder="Prénom" v-model="inscData.firstName" :class="[
+                        'w-full bg-input-bg border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300',
+                        errors.firstName ? 'border-red-500' : 'border-input-border'
+                    ]"
+                        class="w-full bg-input-bg border border-input-border text-text placeholder-text-secondaire px-3.5 py-2.5 rounded-xl outline-none transition-all duration-300 focus:border-secondaire focus:ring-1 focus:ring-secondaire" />
 
-                <button type="submit""
-                    class=" w-full px-3.5 py-2.5 rounded-md font-semibold text-white flex items-center justify-center
+
+                    <button type="submit" class=" w-full px-3.5 py-2.5 rounded-md font-semibold text-white flex items-center justify-center
                     transition-all duration-300 bg-linear-to-r from-principale to-secondaire
                     hover:shadow-[0_0_15px_#9034b080,0_0_15px_#096cfd80]">Inscription</button>
 
-            </form>
+                </form>
 
 
+            </div>
         </div>
     </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+
+const props = defineProps({
+    isOpen: Boolean
+})
+
+const emit = defineEmits(['close'])
 
 let showPassword = ref(true)
 
@@ -53,21 +69,21 @@ const inscData = ref({
     email: "",
     password: "",
     firstName: "",
-    lastName: ""
-
+    isActive: true
 })
 
-let id = ref("")
+const id = ref("")
 
 const getUserByEmail = async () => {
     try {
-        const response = await fetch(`https://money-pie-1.fly.dev/api/v1/users/email/${inscData.email}`)
+        const response = await fetch(`https://money-pie-1.fly.dev/api/v1/users/email/${inscData.value.email}`)
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        id.value = data.id
-
+        id.value = await data.id
+        localStorage.setItem("userId", id.value)
+        console.log("lid dans le local storage est" + localStorage.getItem("userId"))
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
     }
@@ -80,7 +96,7 @@ const post = async (url, dataToSend) => {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(dataToSend.value),
+            body: JSON.stringify(dataToSend),
         });
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
@@ -94,20 +110,64 @@ const post = async (url, dataToSend) => {
     }
 }
 
-const handleSubmit = () => {
-    post(`https://money-pie-1.fly.dev/api/v1/users`, inscData.value)
+const errors = ref({
+    email: false,
+    password: false,
+    firstName: false
+})
 
-    console.log(inscData.value)
+
+
+const handleSubmit = async () => {
+
+
+    errors.value = {
+        email: false,
+        password: false,
+        firstName: false
+    }
+
+    let hasError = false
+
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+
+    if (!inscData.value.email.trim() || !emailRegex.test(inscData.value.email)) {
+        errors.value.email = true
+        hasError = true
+    }
+
+
+    if (!inscData.value.password.trim()) {
+        errors.value.password = true
+        hasError = true
+    }
+
+
+    if (!inscData.value.firstName.trim()) {
+        errors.value.firstName = true
+        hasError = true
+    }
+
+
+    if (hasError) {
+        return
+    }
+
+
+    await post(`https://money-pie-1.fly.dev/api/v1/users`, inscData.value)
+    await getUserByEmail()
+
 
     inscData.value = {
         email: "",
         password: "",
         firstName: "",
-        lastName: ""
+        isActive: true
     }
-    getUserByEmail()
-    localStorage.setItem("userId", id)
-    id.value = ""
+
+    emit('close')
 }
 </script>
 
