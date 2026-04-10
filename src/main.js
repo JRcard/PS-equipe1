3d19a4969395e9c80e57304aca14ac67334a88d4
JRcard <jrm.ricard@gmail.comm>
2026-04-09 20:58:01 -0400

    désinstallation de Pinia. Nous ne l'utillisons pas finalement.

--- src/main.js
+++ src/main.js
@@ -2,12 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/styles.css";
-import { createPinia } from "pinia";
 
 const app = createApp(App);
-const pinia = createPinia();
 
 app.use(router);
-app.use(pinia);
 
 app.mount("#app");
